4910075794081411188eee49eacd9cbc1b631021
Ryan McCann <rmccann2@nd.edu>
2022-12-07 22:19:42 -0500

    finally working

--- mysite/matcher/forms.py
+++ mysite/matcher/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from .models import User
 from django.contrib.auth.forms import UserCreationForm
 
-class UserSignupForm(UserCreationForm):
+class UserSignupForm(forms.ModelForm):
     class Meta:
         model = User
         fields = [
@@ -30,29 +30,29 @@ class UserSignupForm(UserCreationForm):
         # Show only the fields relevant to the selected profile type
         profile_type = self.initial.get('profile_type', User.PROFILE_TYPE_CANDIDATE)
         if profile_type == User.PROFILE_TYPE_CANDIDATE:
-            self.fields = [
-                'profile_type',
-                'first_name',
-                'last_name',
-                'zip_code',
-                'username',
-                'password',
-                'profile_bio',
-                'skills',
-                'github_username',
-                'years_of_experience',
-                'education',
-            ]
+            self.fields.update([
+                ('profile_type', self.fields['profile_type']),
+                ('first_name', self.fields['first_name']),
+                ('last_name', self.fields['last_name']),
+                ('zip_code', self.fields['zip_code']),
+                ('username', self.fields['username']),
+                ('password', self.fields['password']),
+                ('profile_bio', self.fields['profile_bio']),
+                ('skills', self.fields['skills']),
+                ('github_username', self.fields['github_username']),
+                ('years_of_experience', self.fields['years_of_experience']),
+                ('education', self.fields['education']),
+            ])
         elif profile_type == user.PROFILE_TYPE_RECRUITER:
-            self.fields = [
-                'profile_type',
-                'first_name',
-                'last_name',
-                'company_name',
-                'zip_code',
-                'username',
-                'password',
-            ]
+            self.fields.update([
+                ('profile_type', self.fields['profile_type']),
+                ('first_name', self.fields['first_name']),
+                ('last_name', self.fields['last_name']),
+                ('company_name', self.fields['company_name']),
+                ('zip_code', self.fields['zip_code']),
+                ('username', self.fields['username']),
+                ('password', self.fields['password']),
+            ])
             
 class UserLoginForm(forms.ModelForm):
     class Meta:
