fd019a6b05c3565c692ba7d007531ef5cae5777b
Ryan McCann <rmccann2@nd.edu>
2022-12-08 00:26:18 -0500

    Recent changes

--- mysite/matcher/urls.py
+++ mysite/matcher/urls.py
@@ -1,10 +1,12 @@
 # matcher/urls.py
 from django.urls import path
-from .views import UserSignupView, UserLoginView
+from .views import UserSignupView, UserLoginView, CandidateDashboardView, RecruiterDashboardView
 from . import views as matcher_views
 
 urlpatterns = [
     path('', matcher_views.home, name="home"),
     path("signup/", UserSignupView.as_view(), name="signup"),
     path("login/", UserLoginView.as_view(), name="login"),
+    path("rdashboard", RecruiterDashboardView.as_view(), name="recruiter_dashboard"),
+    path("cdashboard", CandidateDashboardView.as_view(), name="candidate_dashboard"),
 ]
