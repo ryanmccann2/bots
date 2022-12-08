c5c1d2178930c6d2808b9df009763147ca5761e5
Ryan McCann <rmccann2@nd.edu>
2022-12-08 01:31:51 -0500

    new changes

--- mysite/matcher/urls.py
+++ mysite/matcher/urls.py
@@ -1,12 +1,14 @@
 # matcher/urls.py
 from django.urls import path
-from .views import UserSignupView, UserLoginView, CandidateDashboardView, RecruiterDashboardView
+from .views import UserSignupView, UserCLoginView, UserRLoginView, CandidateDashboardView, RecruiterDashboardView, CandidateOrRecruiterView
 from . import views as matcher_views
 
 urlpatterns = [
     path('', matcher_views.home, name="home"),
     path("signup/", UserSignupView.as_view(), name="signup"),
-    path("login/", UserLoginView.as_view(), name="login"),
-    path("rdashboard", RecruiterDashboardView.as_view(), name="recruiter_dashboard"),
-    path("cdashboard", CandidateDashboardView.as_view(), name="candidate_dashboard"),
+    path("recruiterlogin/", UserRLoginView.as_view(), name="recruiterlogin"),
+    path("candidatelogin/", UserCLoginView.as_view(), name="candidatelogin"),
+    path("rdashboard/", RecruiterDashboardView.as_view(), name="recruiter_dashboard"),
+    path("cdashboard/", CandidateDashboardView.as_view(), name="candidate_dashboard"),
+    path("corr/", CandidateOrRecruiterView.as_view(), name="candidate_or_recruiter"),
 ]
